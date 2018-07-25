c88c51a08f2950c9eed7ca1beffb54f984ff8c37
Olagunju Adewale <35137159+dewale005@users.noreply.github.com>
2018-07-25 16:17:48 +0100

    final

--- webapp/api/views.py
+++ webapp/api/views.py
@@ -8,3 +8,17 @@ from webapp.api.serializers import UserSerializers, GroupSerializers
 
 # Create your views here.
 
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializers
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializers
+
