7b025858a5cd16595f7305553fa785e1bbde847e
Olagunju Adewale <35137159+dewale005@users.noreply.github.com>
2026-04-17 16:50:11 +0100

    Implement multi-tenant ERP inventory API foundation

--- webapp/api/serializers.py
+++ webapp/api/serializers.py
@@ -1,12 +1,129 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from webapp.api import models
+
 
 class UserSerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
 
+
 class GroupSerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ('url', 'name')
+
+
+class TenantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Tenant
+        fields = '__all__'
+
+
+class PartnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Partner
+        fields = '__all__'
+
+
+class WarehouseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Warehouse
+        fields = '__all__'
+
+
+class ProductCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ProductCategory
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Product
+        fields = '__all__'
+
+
+class StockLevelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.StockLevel
+        fields = '__all__'
+
+
+class StockMovementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.StockMovement
+        fields = '__all__'
+
+
+class PurchaseOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.PurchaseOrder
+        fields = '__all__'
+
+
+class PurchaseOrderLineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.PurchaseOrderLine
+        fields = '__all__'
+
+
+class SalesOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.SalesOrder
+        fields = '__all__'
+
+
+class SalesOrderLineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.SalesOrderLine
+        fields = '__all__'
+
+
+class BillOfMaterialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.BillOfMaterial
+        fields = '__all__'
+
+
+class ManufacturingOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ManufacturingOrder
+        fields = '__all__'
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Employee
+        fields = '__all__'
+
+
+class PayrollEntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.PayrollEntry
+        fields = '__all__'
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Account
+        fields = '__all__'
+
+
+class JournalEntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.JournalEntry
+        fields = '__all__'
+
+
+class JournalLineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.JournalLine
+        fields = '__all__'
+
+
+class LeadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Lead
+        fields = '__all__'
--- webapp/api/tests.py
+++ webapp/api/tests.py
@@ -2,5 +2,28 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from rest_framework.test import APIClient
+from webapp.api import models
 
-# Create your tests here.
+
+class TenantIsolationTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.tenant_a = models.Tenant.objects.create(name='Tenant A', code='TA')
+        self.tenant_b = models.Tenant.objects.create(name='Tenant B', code='TB')
+        self.cat_a = models.ProductCategory.objects.create(tenant=self.tenant_a, name='Cat A')
+        self.cat_b = models.ProductCategory.objects.create(tenant=self.tenant_b, name='Cat B')
+        models.Product.objects.create(tenant=self.tenant_a, sku='SKU-A', name='Product A', category=self.cat_a)
+        models.Product.objects.create(tenant=self.tenant_b, sku='SKU-B', name='Product B', category=self.cat_b)
+
+    def test_products_filtered_by_tenant(self):
+        response = self.client.get('/products/?tenant_id=%s' % self.tenant_a.id)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['sku'], 'SKU-A')
+
+    def test_missing_tenant_returns_empty(self):
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 0)
